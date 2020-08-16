1df7e87b823a6837a9eb2174b64e13393115b0a8
Sonali Agrawal <soagraw@microsoft.com>
2020-08-16 20:13:31 +0530

    reduced space complexity

--- Leetcode Feature chapters/Arrays and Strings/JumpGame.cpp
+++ Leetcode Feature chapters/Arrays and Strings/JumpGame.cpp
@@ -4,21 +4,19 @@ public:
     {
         int n = nums.size();
         vector<bool> jumpsToEnd;
-        vector<int> jumpsToEndIndices;
+        int latestJumpsToEndIndex = n - 1;
         
         for(int i=0; i<n; i++) jumpsToEnd.push_back(false);
         jumpsToEnd[n-1] = true;
-        jumpsToEndIndices.push_back(n-1);
 
         for(int i = n-2; i >= 0; i--)
         {
-            int indexSize = jumpsToEndIndices.size();
-            int distance = jumpsToEndIndices[indexSize-1] - i;
+            int distance = latestJumpsToEndIndex - i;
             if(distance > nums[i]) continue;
             else
             {
                 jumpsToEnd[i] = true; 
-                jumpsToEndIndices.push_back(i);
+                latestJumpsToEndIndex = i;
             }	
         }
 
