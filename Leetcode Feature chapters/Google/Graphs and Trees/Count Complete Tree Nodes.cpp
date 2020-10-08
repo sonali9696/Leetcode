453404a3f2db23ccf64bacce36d516a92e209594
Sonali Agrawal <sonali9696@gmail.com>
2020-10-08 20:18:40 +0530

    complete tree count

---
+++ Leetcode Feature chapters/Google/Graphs and Trees/Count Complete Tree Nodes.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    void preorder(vector<int> &order, TreeNode* root)
+    {
+        if(root == NULL) return;
+
+        order.push_back(root->val);
+        preorder(order, root->left);
+        preorder(order, root->right);
+    }
+	
+    int countNodes(TreeNode* root) {
+        vector<int> order;
+        preorder(order, root); 
+        return order.size();
+    }
+};
+
