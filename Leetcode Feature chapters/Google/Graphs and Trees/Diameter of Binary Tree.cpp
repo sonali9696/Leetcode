ac840c1df8d20a62530f52e5ff305453c4b3a0a1
Sonali Agrawal <sonali9696@gmail.com>
2020-10-21 20:57:42 +0530

    Diameter of Binary Tree

---
+++ Leetcode Feature chapters/Google/Graphs and Trees/Diameter of Binary Tree.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+//good test case -
+//[1,2,3,4,5,null,null,3,null,6,null,null,5,null,7,null,null,null,8]
+
+class Solution {
+private:
+    int diameter = 0;
+    
+public:
+    int countDiameter(TreeNode* root)
+    {
+        if(root == NULL) return -1;
+        if(root->left == NULL && root->right == NULL) return 0;
+
+        int l,r;
+        if(root->left == NULL) l = -1;
+        else l = countDiameter(root->left);
+
+        if(root->right == NULL) r = -1;
+        else r = countDiameter(root->right);
+
+        int diameterThroughCurrentNode = l + r + 2;
+        if(diameterThroughCurrentNode > diameter)
+            diameter = diameterThroughCurrentNode;
+        
+        return max(l,r)+1;
+    }
+    
+    int diameterOfBinaryTree(TreeNode* root) {
+        int ans = countDiameter(root);
+        if(diameter < ans) diameter = ans;
+        
+        return diameter;
+    }
+};
+
