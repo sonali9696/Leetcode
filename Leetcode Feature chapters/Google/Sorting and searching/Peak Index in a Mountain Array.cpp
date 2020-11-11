ac700781ef280841d4d671b2eb6dd002d003b4c5
Sonali Agrawal <sonali9696@gmail.com>
2020-11-11 20:23:22 +0530

    optimized

--- Leetcode Feature chapters/Google/Sorting and searching/Peak Index in a Mountain Array.cpp
+++ Leetcode Feature chapters/Google/Sorting and searching/Peak Index in a Mountain Array.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
+        int n = arr.size();
+        int low = 0, high = n-1, mid;
+        int peak;
+        while(low <= high)
+        {
+            mid = (low+high)/2;
+            if((mid >= 1 && arr[mid] > arr[mid-1]) && (mid <= n-2 && arr[mid] > arr[mid+1]))
+            {
+                peak = mid;
+                break;
+            }
+            else if(mid < 1 || (arr[mid] > arr[mid-1])) //left side -> increasing side
+            {
+                low = mid;
+            }
+            else if(mid > n-2 || (arr[mid] > arr[mid+1])) //right side -> decreasing side
+            {
+                high = mid;
+            }
+        }
+        
+        return peak;
+    }
+};
+
+/*class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
         int peakMountain = arr[0], peakInd = 0, n = arr.size();
         for(int i=1; i<n; i++)
         {
@@ -12,4 +40,4 @@ public:
         }
         return peakInd;
     }
-};
+};*/
