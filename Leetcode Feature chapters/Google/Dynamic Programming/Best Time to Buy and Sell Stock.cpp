2efa724238c6553d6a56a6ef544943f394f8d77c
Sonali Agrawal <sonali9696@gmail.com>
2020-11-17 21:38:09 +0530

    optimized

--- Leetcode Feature chapters/Google/Dynamic Programming/Best Time to Buy and Sell Stock.cpp
+++ Leetcode Feature chapters/Google/Dynamic Programming/Best Time to Buy and Sell Stock.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int profit = 0, n = prices.size(), left = 1, right = n-2;
+        int profit = 0, n = prices.size(), i = 1;
 	    if(n == 0) return 0;
-        int leftmin = prices[0], rightmax = prices[n-1];
-        int leftProfit = 0, rightProfit=0;
-        
-        while(left < n && right >=0)
+        int currmin = prices[0];
+        while(i < n)
         {
-            if(prices[left] < leftmin) leftmin = prices[left];
-            if(prices[left] - leftmin > leftProfit) leftProfit = prices[left] - leftmin;
-
-            if(prices[right] > rightmax) rightmax = prices[right];
-            if(rightmax - prices[right] > rightProfit) rightProfit = rightmax - prices[right];
-
-            int currMaxProfit = max(leftProfit, rightProfit);
-            if(profit < currMaxProfit) profit = currMaxProfit;
-
-            left++;
-            right--;
+            if(prices[i] < currmin) currmin = prices[i];
+            if(prices[i] - currmin > profit) profit = prices[i] - currmin;
+            i++;
         }
         
         return profit;
