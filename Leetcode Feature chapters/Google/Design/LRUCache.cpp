f1cf380e6839f61ddd4720cd5ac77f2fc7976820
Sonali Agrawal <sonali9696@gmail.com>
2020-11-28 18:22:03 +0530

    optimized

--- Leetcode Feature chapters/Google/Design/LRUCache.cpp
+++ Leetcode Feature chapters/Google/Design/LRUCache.cpp
@@ -1,67 +1,78 @@
 class LRUCache {
 private:
+	list<int> sequence;
+	map<int,int> cache; //key,value
+	map<int,list<int>::iterator> cacheToSeq; //key,iterator to element in vector sequence 
+//(not mapping to index as when we push new element in front, index of every element changes)
+//absolute index changes, relative is same hence using iterator
 	int n;
-	map<int,int> cache;
-	map<int,int> lastUsed; //sequence of usage,key
-	long long int seqNo = 0;
+	int currentSize;
 
 public:
     LRUCache(int capacity) {
         n = capacity;
+        currentSize = 0;
+    }
+
+    void pushToFront(int key)
+    {
+            sequence.push_front(key);
+            cacheToSeq[key] = sequence.begin();
+    }
+
+    void moveToFront(int key)
+    {
+            list<int>::iterator it = cacheToSeq[key];
+            sequence.erase(it);
+            pushToFront(key);
     }
 
     int get(int key) {
         if(cache.find(key) == cache.end()) return -1;
-        else 
-        {
-            lastUsed[key] = seqNo++;
-            /*cout<<"*********"<<key<<endl;
-            map<int,int>::iterator it2;
-            for(it2 = cache.begin(); it2 != cache.end(); it2++) cout<<it2->first<<" "<<it2->second<<endl;
-            cout<<endl;
-            for(it2 = lastUsed.begin(); it2 != lastUsed.end(); it2++) cout<<it2->first<<" "<<it2->second<<endl;
-            cout<<"********"<<endl;*/
-            return cache[key];
-        }
-    }
 
-    void put(int key, int value) {
-        if(cache.find(key) != cache.end())
-        {
-            cache[key] = value;
-            lastUsed[key] = seqNo++;
-            return;
-        }
-        if(cache.size() < n)
-        {
-            cache[key] = value;
-            lastUsed[key] = seqNo++;
-        }
-        else //evict LRU then push
-        {
-            int keyToBeReplaced;
-            int minSeq = INT_MAX;
-            map<int,int>::iterator it;
-            for(it = lastUsed.begin(); it != lastUsed.end(); it++)
-            {
-                if(it->second < minSeq) 
-                {
-                    minSeq = it->second;
-                    keyToBeReplaced = it->first;
-                }
-            }
-            //cout<<"keyToBeReplaced="<<keyToBeReplaced<<endl;
-            cache.erase(keyToBeReplaced);
-            cache[key] = value;
-            lastUsed.erase(keyToBeReplaced);
-            lastUsed[key] = seqNo++;
-        }
+        moveToFront(key);
         
-        /*cout<<"*********"<<key<<" "<<value<<endl;
+        /*cout<<"*********"<<key<<endl;
         map<int,int>::iterator it2;
         for(it2 = cache.begin(); it2 != cache.end(); it2++) cout<<it2->first<<" "<<it2->second<<endl;
         cout<<endl;
-        for(it2 = lastUsed.begin(); it2 != lastUsed.end(); it2++) cout<<it2->first<<" "<<it2->second<<endl;
+        for(auto v : sequence) cout<<v<<endl;
+        cout<<"********"<<endl;*/
+        
+        return cache[key];
+    }
+
+    void put(int key, int value) {
+        //cout<<"*********"<<key<<" "<<value<<endl;
+        if(cache.find(key) != cache.end())
+        {
+			//update value
+            cache[key] = value;
+            moveToFront(key);
+            return;
+        }
+
+        if(currentSize < n)
+        {
+            currentSize++;
+        }
+        else
+        {
+            //remove least recently used
+            int old = sequence.back();
+            //cout<<"removing "<<old<<endl;
+            sequence.pop_back();
+            cache.erase(old);
+            cacheToSeq.erase(old);
+        }
+		
+        pushToFront(key);
+        cache[key] = value;
+        
+        /*map<int,int>::iterator it2;
+        for(it2 = cache.begin(); it2 != cache.end(); it2++) cout<<it2->first<<" "<<it2->second<<endl;
+        cout<<endl;
+        for(auto v : sequence) cout<<v<<endl;
         cout<<"********"<<endl;*/
     }
 };
