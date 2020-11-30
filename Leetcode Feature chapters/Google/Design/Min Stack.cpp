d724d19cc724a4e1f0d325cfbc03a76b9c1f55b1
Sonali Agrawal <sonali9696@gmail.com>
2020-11-30 12:20:35 +0530

    optimized space by storing less mins

--- Leetcode Feature chapters/Google/Design/Min Stack.cpp
+++ Leetcode Feature chapters/Google/Design/Min Stack.cpp
@@ -1,33 +1,43 @@
 class MinStack {
-class MinStack {
 private:
-    stack<pair<int,int> > st;
+    stack<int> st;
+    stack<int> minTracker;
     //at every point, elements under x are unchanged so min is min till x-1 and xth element.
+    //here we aren't pushing to minTracker if no new min found, except when x=latest min because 
+    //we min might occur at 2 diff points in stack, we don't want to pop it and then give wrong min for other elements
+    //eg: {6,6} are the 2 latest pushes to stack. If we store 6 only once in min, then for 2nd pop it will give some other min as 6 is already popped.
     
 public:
     /** initialize your data structure here. */
     MinStack() {
     }
     
-    void push(int x) {
+    void push(int x) {        
         int currMin;
-        if(!st.empty()) currMin = st.top().second;
+        if(!minTracker.empty()) currMin = minTracker.top();
         else currMin = x;
-        if(currMin < x) st.push(make_pair(x,currMin));
-        else st.push(make_pair(x,x));
+        if(currMin >= x) minTracker.push(x); //else currMin already in min stack correctly
+        //if stack is empty, x will be pushed by above logic
+
+        st.push(x);
     }
     
     void pop() {
-        if(!st.empty()) st.pop();
+        if(!st.empty()) 
+        {
+            if(st.top() == minTracker.top()) minTracker.pop();
+            st.pop();
+        }
     }
     
     int top() {
         if(st.empty()) return -1;
-        return st.top().first;
+        return st.top();
     }
     
     int getMin() {
-        return st.top().second;
+        if(minTracker.empty()) return -1;
+        return minTracker.top();
     }
 };
 
