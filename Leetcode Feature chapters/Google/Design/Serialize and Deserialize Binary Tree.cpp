25c81b151b9bdb5a9a16717e6084b8b8bdb0c91b
Sonali Agrawal <sonali9696@gmail.com>
2020-12-03 16:05:15 +0530

    added dfs solution - slower but shorter

--- Leetcode Feature chapters/Google/Design/Serialize and Deserialize Binary Tree.cpp
+++ Leetcode Feature chapters/Google/Design/Serialize and Deserialize Binary Tree.cpp
@@ -9,6 +9,37 @@
  */
 
 
+//Short DFS solution - 
+/*class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        if (root == NULL) return "#";
+        return to_string(root->val) + " " + serialize(root->left) + " " + serialize(root->right);
+    }
+
+    TreeNode* deserialize(string data) {
+        if (data == "#") return NULL;
+        stringstream s(data);
+        return makeDeserialize(s);
+    }
+    
+    TreeNode* makeDeserialize(stringstream& s) {
+        string str;
+        s >> str;
+        //cout<<str<<endl;
+        if (str.compare("#") == 0) {
+            return NULL;
+        } else {
+            TreeNode* root = new TreeNode(stoi(str));
+            root->left = makeDeserialize(s);
+            root->right = makeDeserialize(s);
+            return root;
+        }
+    }
+};*/
+
 class Codec {
 public:
     // Encodes a tree to a single string.
