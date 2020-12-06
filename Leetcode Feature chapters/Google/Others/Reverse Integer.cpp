039ee10215b86a61824e238e4b40c95eadb690a5
Sonali Agrawal <sonali9696@gmail.com>
2020-12-06 19:07:31 +0530

    alternative approach

--- Leetcode Feature chapters/Google/Others/Reverse Integer.cpp
+++ Leetcode Feature chapters/Google/Others/Reverse Integer.cpp
@@ -4,6 +4,10 @@ public:
         long long int reversedX = 0;
         while(x != 0)
         {
+            //Alternative approach  -
+            if (reversedX > INT_MAX/10 || (reversedX == INT_MAX / 10 && (x%10) > 7)) return 0;
+            if (reversedX < INT_MIN/10 || (reversedX == INT_MIN / 10 && (x%10) < -8)) return 0;
+            
             reversedX = reversedX*10 + (x%10);
             x /= 10;
         }
