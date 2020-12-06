3baacfb2e2d885fd932e9eddcd6f48d073467f29
Sonali Agrawal <sonali9696@gmail.com>
2020-12-06 19:05:38 +0530

    shorter solution

--- Leetcode Feature chapters/Google/Others/Reverse Integer.cpp
+++ Leetcode Feature chapters/Google/Others/Reverse Integer.cpp
@@ -4,14 +4,11 @@ public:
         long long int reversedX = 0;
         while(x != 0)
         {
-            long long int temp = reversedX*10 + (x%10);
-            if(temp > (pow(2,31)-1) || temp < -(pow(2,31))) return 0;
-            reversedX = temp;
-            
+            reversedX = reversedX*10 + (x%10);
             x /= 10;
         }
             
-        if(reversedX > (pow(2,31)-1) || reversedX < -(pow(2,31))) return 0; 
+        if(reversedX > INT_MAX || reversedX < INT_MIN) return 0; 
         return reversedX;
     }
 };
