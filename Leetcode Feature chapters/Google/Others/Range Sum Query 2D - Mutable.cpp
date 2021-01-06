f37faa5cd7496dafd2c390c081ab99c2f2d2e7c0
Sonali Agrawal <sonali9696@gmail.com>
2021-01-06 21:01:43 +0530

    Range Sum Query 2D - Mutable

---
+++ Leetcode Feature chapters/Google/Others/Range Sum Query 2D - Mutable.cpp
@@ -0,0 +1,34 @@
+class NumMatrix {
+private:
+	vector<vector<int>> m;
+public:
+    NumMatrix(vector<vector<int>>& matrix) {
+        m = matrix;
+    }
+    
+    void update(int row, int col, int val) {
+        m[row][col] = val;
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        int sum = 0;
+        for(int i=row1; i<= row2; i++)
+		{
+			for(int j=col1; j<=col2; j++)
+			{
+				sum += m[i][j];
+			}
+		}
+		return sum;
+    }
+};
+
+
+
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * obj->update(row,col,val);
+ * int param_2 = obj->sumRegion(row1,col1,row2,col2);
+ */
