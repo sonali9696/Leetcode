c248559eb9bb8e6daf66c091c43893951fd53b92
Sonali Agrawal <sonali9696@gmail.com>
2021-02-01 20:21:28 +0530

    optimized

--- Leetcode Medium Google questions/Unique Word Abbreviation.cpp
+++ Leetcode Medium Google questions/Unique Word Abbreviation.cpp
@@ -1,7 +1,8 @@
 class ValidWordAbbr {
 private:
-	map<string,set<string> > abbreviations;
+	map<string,bool> abbreviations; //abbrevation, 1 or more than 1 words
     //set is used so that duplicates are not stored
+    set<string> dict;
 
 	string abbreviate(string word)
 	{
@@ -16,11 +17,16 @@ private:
 
 public:
     ValidWordAbbr(vector<string>& dictionary) {
-       	int n = dictionary.size();
-		for(int i=0; i<n; i++)
+        //to remove duplicates-
+        for(string w : dictionary) dict.insert(w);
+        
+        set<string>::iterator it;
+		for(it = dict.begin(); it != dict.end(); it++)
 		{
-			string abbr = abbreviate(dictionary[i]);
-			abbreviations[abbr].insert(dictionary[i]);
+			string abbr = abbreviate(*it);
+            if(abbreviations.find(abbr) == abbreviations.end())
+                abbreviations[abbr] = true;
+			else abbreviations[abbr] = false; //two words with same abbr
 		}
     }
     
@@ -29,16 +35,25 @@ public:
 		if(abbreviations.find(abbr) == abbreviations.end()) return true;
 		else
 		{
-			set<string> existingWords = abbreviations[abbr];
+			bool singleWord = abbreviations[abbr];
 			//if 2 words say deer,door have same abbreviation and word is deer then it is saying to return false maybe because of door so if 2 or more words have same abbr then not possible to check that condition so all words in vector has to be this word
             
-            if(existingWords.size() > 1) return false;
+            if(singleWord == false) return false;
+            else
+            {
+                //check if word is in dict 
+                //if yes means it is the only word
+                if(dict.count(word) != 0) return true;
+                else return false;
+            }
+            
+            /*if(existingWords.size() > 1) return false;
             else
             {
                 auto it = existingWords.begin();
                 if(*it == word) return true; 
                 else return false;
-            }
+            }*/
             
             /*int n = existingWords.size();
             for(int i=0; i<n; i++)
