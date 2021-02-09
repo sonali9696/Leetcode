0c6f88875e41d125476e31de7b0205501e2ef0e4
Sonali Agrawal <sonali9696@gmail.com>
2021-02-09 17:03:21 +0530

    Zigzag Iterator

---
+++ Leetcode Medium Google questions/Zigzag Iterator.cpp
@@ -0,0 +1,46 @@
+class ZigzagIterator {
+private:
+	int i, j, m, n;
+	int currentVector;
+    vector<int> vec1, vec2;
+public:
+    ZigzagIterator(vector<int>& v1, vector<int>& v2) 
+    {
+        m = v1.size();
+        n = v2.size();
+        vec1 = v1;
+        vec2 = v2;
+        i=0, j=0; 
+        if(i < m) currentVector = 1;
+        else currentVector = 2;
+    }
+
+    int next() {
+		if(i >= m) currentVector = 2;
+        if(j >= n) currentVector = 1; 
+
+        if(currentVector == 1)
+        {
+            if(j < n) currentVector = 2;
+            return vec1[i++];
+        }
+        else if(currentVector == 2)
+        {
+            if(i < m) currentVector = 1;
+            return vec2[j++];
+        }
+        
+        return vec1[i++]; //dummy value to return something in all paths
+    }
+
+    bool hasNext() {
+         if(i >= m && j >= n) return false;
+         else return true;
+    }
+};
+
+/**
+ * Your ZigzagIterator object will be instantiated and called as such:
+ * ZigzagIterator i(v1, v2);
+ * while (i.hasNext()) cout << i.next();
+ */
