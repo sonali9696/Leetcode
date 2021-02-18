12f0135966eea2032278b0462fbabe4229d08033
Sonali Agrawal <sonali9696@gmail.com>
2021-02-18 19:53:00 +0530

    optimized

--- Leetcode Google questions/My Calendar I.cpp
+++ Leetcode Google questions/My Calendar I.cpp
@@ -1,5 +1,24 @@
 class MyCalendar {
 private:
+    map<int,int> bookings;  
+public:
+    MyCalendar() {
+    }
+    
+    bool book(int start, int end) {
+        if(bookings.find(start) != bookings.end()) return false; //2 events cant start together
+        
+        auto nextEvent = bookings.lower_bound(start); //lower_bound means event >=s. =s is handled so this is the event after s
+        if(nextEvent != bookings.end() && nextEvent->first < end) return false; //overlap with next event
+        if(nextEvent != bookings.begin() && (--nextEvent)->second > start) return false; //overlap with prev event
+       
+        bookings[start] = end; //map automatically stores at correct place to keep it sorted
+        return true;
+    }
+};
+
+/*class MyCalendar {
+private:
     vector<pair<int,int> > bookings;  
 public:
     MyCalendar() {
@@ -15,4 +34,10 @@ public:
         bookings.push_back(make_pair(start,end));
         return true;
     }
-};
+};*/
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
