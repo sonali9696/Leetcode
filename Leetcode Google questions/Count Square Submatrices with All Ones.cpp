35bf16eaa9745f09abac8d20e320428aedc90d35
Sonali Agrawal <sonali9696@gmail.com>
2021-02-19 18:46:33 +0530

    DP solution

--- Leetcode Google questions/Count Square Submatrices with All Ones.cpp
+++ Leetcode Google questions/Count Square Submatrices with All Ones.cpp
@@ -1,4 +1,31 @@
 class Solution {
+public:
+   	int countSquares(vector<vector<int>>& matrix) {
+       	int m = matrix.size(), n = matrix[0].size(), ans = 0;
+
+        //storing dp in the same matrix array for space complexity O(1)
+        //dp(i,j) = side of largest square possible with (i,j) as bottom right corner
+        //note square of side 3 means 3*3 is filled with 1 not just border of square
+        //so dp(i,j) = min(dp(i-1,j),dp(i-1,j-1),dp(i,j-1)) + 1
+
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {	
+                //if i=0/j=0, it will be 1 or 0 based on A[i][j] so leave as is
+                if(i != 0 && j != 0 && matrix[i][j] == 1)
+                {
+                    matrix[i][j] = min(matrix[i-1][j],min(matrix[i-1][j-1],matrix[i][j-1])) + 1;
+                }
+                ans += matrix[i][j];
+            }
+        }
+        return ans;
+    }
+};
+
+
+/*class Solution {
 private:
 	int m, n, maxPossibleSquareSize;
 public:
@@ -42,5 +69,5 @@ public:
 
         return numOfSquares;
     }
-};
+};*/
 
