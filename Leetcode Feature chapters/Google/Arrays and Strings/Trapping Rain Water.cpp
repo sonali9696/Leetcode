9f8e9fe11250bcfa1079a070182549d8a5f2efb2
Sonali Agrawal <sonali9696@gmail.com>
2021-02-23 21:45:04 +0530

    optimized

--- Leetcode Feature chapters/Google/Arrays and Strings/Trapping Rain Water.cpp
+++ Leetcode Feature chapters/Google/Arrays and Strings/Trapping Rain Water.cpp
@@ -1,5 +1,26 @@
 class Solution {
 public:
+    int trap(vector<int>& height)
+    {
+        int left = 0, right = height.size() - 1;
+        int ans = 0;
+        int left_max = 0, right_max = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                height[left] >= left_max ? (left_max = height[left]) : ans += (left_max - height[left]);
+                ++left;
+            }
+            else {
+                height[right] >= right_max ? (right_max = height[right]) : ans += (right_max - height[right]);
+                --right;
+            }
+        }
+        return ans;
+    }
+};
+
+/*class Solution {
+public:
     int trap(vector<int>& height) {
         int n = height.size(), waterQuantity = 0;
         if(n <= 2) return 0;
@@ -23,4 +44,4 @@ public:
     }
 
 
-};
+};*/
