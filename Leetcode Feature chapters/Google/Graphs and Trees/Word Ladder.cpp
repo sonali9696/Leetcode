3a70db9d26c6ca285d36a6ff4fca47e071e45b95
Sonali Agrawal <sonali9696@gmail.com>
2021-02-24 18:36:07 +0530

    optimized

--- Leetcode Feature chapters/Google/Graphs and Trees/Word Ladder.cpp
+++ Leetcode Feature chapters/Google/Graphs and Trees/Word Ladder.cpp
@@ -14,72 +14,85 @@ void findSimilarWords(string s, int l, map<string,vector<string>> &similarWords)
 
 }
 
+int bfsTraverse(queue<pair<string,int>> &bfs, map<string, int> &visited, map<string, int> &visitedOtherDirection, map<string,vector<string>> &similarWords)
+{
+    string s = (bfs.front()).first;
+    int currentLevel = (bfs.front()).second;
+    bfs.pop();
+
+    //cout<<"popped "<<s<<endl;
+
+    //find neighbours and push them
+    string temp = s;
+    int l = s.length();
+    for(int i=0; i<l; i++)
+    {
+        char currentChar = temp[i];
+        temp[i] = '*';
+
+        //cout<<"for format - "<<temp<<endl;
+        vector<string> nextWords = similarWords[temp];
+
+        for(int j=0; j<nextWords.size(); j++)
+        {
+            if(nextWords[j] == s) continue;
+            
+            if(visitedOtherDirection.find(nextWords[j]) != visitedOtherDirection.end())
+            {
+                return currentLevel + (visitedOtherDirection[nextWords[j]]);
+            }
+            
+            if(visited.find(nextWords[j]) == visited.end())
+            {
+                
+                bfs.push(make_pair(nextWords[j],currentLevel+1));
+                visited[nextWords[j]] = currentLevel+1;
+                //cout<<"pushed "<<nextWords[j]<<" with level "<<currentLevel+1<<endl;
+            }
+        }
+
+        temp[i] = currentChar;
+    }
+    
+    return -1;
+}
+
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        
+        //Note: beginWord need not be in dictionary but end word has to be
+        if(find(wordList.begin(), wordList.end(), endWord) == wordList.end()) return 0;
+
+        int l2 = wordList.size();
         map<string,vector<string>> similarWords;
         
         if(beginWord == endWord) return 1;
 
         int l = beginWord.length();
-        int l2 = wordList.size();
         findSimilarWords(beginWord, l, similarWords);
 
-        for(int i=0; i < l2; i++)findSimilarWords(wordList[i], l, similarWords);
-
-        /*map<string,vector<string>>::iterator it;
-        for(it = similarWords.begin(); it != similarWords.end(); ++it)
+        for(int i=0; i < l2; i++)
         {
-            cout<<it->first<<endl;
-            vector<string> tempV = it->second;
-            for(int i=0; i<tempV.size();i++) cout<<tempV[i]<<" "<<endl;
-            cout<<"*********"<<endl;
-        }*/
-        
-        map <string, bool> visited;
-        for(int i=0; i<l2; i++)
-        {
-            visited[wordList[i]] = false;
+            findSimilarWords(wordList[i], l, similarWords);
         }
 
-        queue<pair<string,int>> bfs;
-        bfs.push(make_pair(beginWord,1));
-        visited[beginWord] = true;
-        int currentLevel = 1;
+        map<string, int> visitedBegin, visitedEnd; //word, level
+
+        queue<pair<string,int>> bfsBegin;
+        bfsBegin.push(make_pair(beginWord,1));
+        visitedBegin[beginWord] = 1;
         
-        while(!bfs.empty())
+        queue<pair<string,int>> bfsEnd;
+        bfsEnd.push(make_pair(endWord,1));
+        visitedEnd[endWord] = 1;
+        
+        while(!bfsBegin.empty() && !bfsEnd.empty())
         {
-            string s = (bfs.front()).first;
-            currentLevel = (bfs.front()).second;
-            if(s == endWord) return currentLevel;
-            bfs.pop();
-
-            //cout<<"popped "<<s<<endl;
+            int ans = bfsTraverse(bfsBegin, visitedBegin, visitedEnd, similarWords);
+            if(ans > -1) return ans;
             
-            //find neighbours and push them
-            string temp = s;
-            for(int i=0; i<l; i++)
-            {
-                char currentChar = temp[i];
-                temp[i] = '*';
-
-                //cout<<"for format - "<<temp<<endl;
-                vector<string> nextWords = similarWords[temp];
-
-                for(int j=0; j<nextWords.size(); j++)
-                {
-                    if(visited[nextWords[j]] == false)
-                    {
-                        if(nextWords[j] == endWord) return currentLevel+1;
-                        bfs.push(make_pair(nextWords[j],currentLevel+1));
-                        visited[nextWords[j]] = true;
-                        //cout<<"pushed "<<nextWords[j]<<" with level "<<currentLevel+1<<endl;
-                    }
-                }
-
-                temp[i] = currentChar;
-            }
+            ans = bfsTraverse(bfsEnd, visitedEnd, visitedBegin, similarWords);
+            if(ans > -1) return ans;   
         }
 
         return 0;
