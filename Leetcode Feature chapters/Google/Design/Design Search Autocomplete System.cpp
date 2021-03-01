9382a639f426f2f7340cbf3668e8c71ad30522ea
Sonali Agrawal <sonali9696@gmail.com>
2021-03-01 22:34:54 +0530

    optimized

--- Leetcode Feature chapters/Google/Design/Design Search Autocomplete System.cpp
+++ Leetcode Feature chapters/Google/Design/Design Search Autocomplete System.cpp
@@ -1,142 +1,86 @@
-class Trie{
-private:
-	struct TrieNode{
-		bool isLeaf = false;
-		int times = 0;
-		TrieNode* children[27];
-    };
-
-    TrieNode* root;
-
-public:
-	Trie()
-    {
-        root = new TrieNode();
-        for(int i=0; i<27; i++) root->children[i] = NULL;
-    }
-
-    void addStringToTrie(string s, int count)
-    {
-        //cout<<"to add:"<<s<<endl;
-        int l = s.length(),index;
-        TrieNode* p = root;
-        for(int i=0; i<l; i++)
-        {
-            if(s[i] == ' ') index = 26;
-            else index = s[i] - 'a';
-            if(p->children[index] == NULL)
-            {
-                TrieNode *newNode = new TrieNode();
-                p->children[index] = newNode;
-                for(int i=0; i<27; i++) newNode->children[i] = NULL;
-                //cout<<"new node created "<<index<<" "<<s[i]<<endl;
-            }
-
-            p = p->children[index];
-        }
-
-        p->isLeaf = true;
-        p->times += count;
-        //cout<<"times="<<p->times<<endl;
-        //cout<<"********************"<<endl;
-    }
-
-    //search prefix in trie and return list of all sentences
-    vector<pair<string,int> > findPrefixInTrie(string searchTerm)
-    {
-        int l = searchTerm.length();
-        vector<pair<string,int> > suggestions;
-        TrieNode* p = root;
-        for(int i=0; i<l; i++)	
-        {
-            int index;
-            if(searchTerm[i] == ' ') index = 26;
-            else index = searchTerm[i] - 'a';
-            if(p->children[index] == NULL) return suggestions;
-            else p = p->children[index];
-        }
-
-        if(p->isLeaf == true) suggestions.push_back(make_pair(searchTerm,p->times));
-
-        //get all sentences from here
-        for(int i=0; i<27; i++)
-        {
-            if(p->children[i] != NULL)
-            {
-                char c;
-                if(i < 26) c = i + 'a';
-                else c = ' ';
-                reachLeaf(searchTerm+c,suggestions,p->children[i]);
-            }
-        }
-
-        return suggestions;
-    }
-
-    void reachLeaf(string currString,vector<pair<string,int> > &suggestions, TrieNode* p)
-    {
-        if(p->isLeaf == true) suggestions.push_back(make_pair(currString,p->times));
-
-        int count = 0;
-        for(int i=0; i<27; i++)
-        {
-            if(p->children[i] != NULL) 
-            {
-                char c;
-                if(i < 26) c = i + 'a';
-                else c = ' ';
-                reachLeaf(currString+c,suggestions,p->children[i]);
-            }
-        }
-    }
-};
+//Match the prefix using a Trie and do a DFS to find all the competing strings and put them in a priority queue. Initially I created a map of (string, count) at each TrieNode but it's very inefficient as far as memory's concerned. Also, I search the trie from the previous character encountered and reset the starting TrieNode for new input sequence.
 
 class AutocompleteSystem {
-private:
-	string searchTerm;
-    Trie t;
-public:
-    AutocompleteSystem(vector<string>& sentences, vector<int>& times) {
-        searchTerm = "";
-        int l = sentences.size();
-        for(int i=0; i<l; i++) t.addStringToTrie(sentences[i],times[i]);
+    
+    class TrieNode{
+        public:
+            unordered_map<char, TrieNode*> child;
+            string str;
+            int count;
+            TrieNode(): str(""), count(0) {}
+    };
+    
+    void insert(string& s, TrieNode* root, int times){
+        TrieNode* curr = root;
+        for (int i=0;i<s.size();i++){
+            if (!curr->child.count(s[i]))
+                curr->child[s[i]] = new TrieNode();
+            curr = curr->child[s[i]];
+        }
+        curr->count += times;
+        curr->str = s;
     }
     
-    static bool sortByTimes(const pair<string,int> &p1, const pair<string,int> &p2)
-	{
-		if(p1.second == p2.second) return p1.first < p2.first;
-		else return p1.second > p2.second;
+public:
+    void dfs(TrieNode* temp){
+        if (temp->str != "") q.push({temp->str, temp->count});
+        
+        for (auto& ele: temp->child){
+            dfs(ele.second);
+        }
     }
-
+    
+    struct comp{
+        bool operator() (pair<string, int>& a, pair<string, int>& b){
+            return a.second<b.second || a.second==b.second && a.first>b.first;
+        }
+    };
+    
+    priority_queue<pair<string, int>, vector<pair<string, int> >, comp> q;
+        
+    TrieNode* root, *curr;
+    AutocompleteSystem(vector<string> sentences, vector<int> times) {
+        root = new TrieNode();
+        for (int i=0;i<sentences.size();i++){
+            insert(sentences[i], root, times[i]);
+        }
+        curr = root;
+    }
+    
+    
+    string s="";
     vector<string> input(char c) {
-       	if(c == '#')
-		{
-            t.addStringToTrie(searchTerm,1);
-			searchTerm = "";
-			vector<string> empty;
-			return empty;
-		}
-		
-		searchTerm += c;
-		vector<pair<string,int> > suggestions = t.findPrefixInTrie(searchTerm);
-	
-		//sort vector acc to times, and ascii
-		sort(suggestions.begin(),suggestions.end(),sortByTimes);
-
-		//take first 3 or full if size < 3
-		vector<string> result;
-
-		//append to result
-        int size = 3 < suggestions.size()?3:suggestions.size();
-		for(int i=0; i < size; i++) result.push_back(suggestions[i].first);
-		
-		return result;
+        q = priority_queue<pair<string, int>, vector<pair<string, int> >, comp>();
+        if (c=='#'){
+            insert(s, root, 1);
+            s="";
+            curr = root; //start searching from the beginning node for the next sentence
+            return {};
+        }
+        s += c;
+        if (curr && curr->child.count(c)){
+            curr = curr->child[c];
+        }else{
+            curr = NULL; //curr node is null so empty result for any further characters in current input 
+            return {};
+        }
+        
+        if (curr->str != "") q.push({curr->str, curr->count});
+        for (auto& ele: curr->child){
+            dfs(ele.second);
+        }
+        
+        vector<string> res;
+        while (!q.empty() && res.size()<3){
+            res.push_back(q.top().first);
+            q.pop();
+        }
+        
+        return res;
     }
 };
-
 /**
  * Your AutocompleteSystem object will be instantiated and called as such:
  * AutocompleteSystem* obj = new AutocompleteSystem(sentences, times);
  * vector<string> param_1 = obj->input(c);
  */
-
