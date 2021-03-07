275ab1e2a6babbd86f471255f40bea23307cdc14
Sonali Agrawal <sonali9696@gmail.com>
2021-03-07 12:41:37 +0530

    function stack space optimized

--- Leetcode Google questions/Surrounded Regions.cpp
+++ Leetcode Google questions/Surrounded Regions.cpp
@@ -3,12 +3,14 @@ private:
 	int m,n;
 public:
 	void dfs(vector<vector<char>>& board, int i, int j){
-		if(i < 0 || i >= m || j < 0 || j >=n || board[i][j] != 'O') return;
 		board[i][j] = '#'; //marking as visited
-		dfs(board, i-1, j);
-		dfs(board, i+1, j);
-		dfs(board, i, j-1);
-		dfs(board, i, j+1);
+		if(i-1 >= 0 && board[i-1][j] == 'O') dfs(board, i-1, j);
+		if(i+1 < m && board[i+1][j] == 'O') dfs(board, i+1, j);
+		if(j - 1 >= 0 && board[i][j-1] == 'O') dfs(board, i, j-1);
+		if(j+1 < n && board[i][j+1] == 'O')dfs(board, i, j+1);
+        //Note: checking boundary condition of i,j before calling function
+        //reduces function overhead cost hence improves space complexity
+        //due to lesser space by function stack
     }
 
     void solve(vector<vector<char>>& board) {
