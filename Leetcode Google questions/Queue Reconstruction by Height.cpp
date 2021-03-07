ecd18400ba813197a0fa08d7b5075a2388476046
Sonali Agrawal <sonali9696@gmail.com>
2021-03-07 19:54:01 +0530

    adding another approach

--- Leetcode Google questions/Queue Reconstruction by Height.cpp
+++ Leetcode Google questions/Queue Reconstruction by Height.cpp
@@ -1,3 +1,25 @@
+//if we sort people by descending height and if same height then ascending k value
+//then we can insert people one by one by index = k values
+/*class Solution{
+public:
+    static bool sortPeople(const vector<int>& a, const vector<int>& b){
+        if(a[0] == b[0]) return a[1] < b[1];
+        else return a[0] > b[0];
+    }
+        
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people){
+        sort(people.begin(), people.end(), sortPeople);
+        vector<vector<int>> q;
+        
+        for(vector<int> p : people){
+            q.insert(q.begin()+p[1],p);
+        }
+        
+        return q;
+    }
+};*/
+
+
 class Solution {
 public:
     static bool sortPeople(const vector<int>& a, const vector<int>& b){
