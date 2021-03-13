235013ac81b13d404ff0a5e53ec7906d35d267f6
Sonali Agrawal <sonali9696@gmail.com>
2021-03-13 21:21:44 +0530

    optimized by kmp solution

--- Leetcode Google questions/Repeated String Match.cpp
+++ Leetcode Google questions/Repeated String Match.cpp
@@ -1,7 +1,72 @@
 class Solution {
 public:
-    int repeatedStringMatch(string A, string B) {
+    int repeatedStringMatch(string a, string b) {
+        if (b.empty()) return 0;
         
+        std::vector<int> t; //lps array
+        t.reserve(b.size());
+        int i = 0;
+        int j = 1;
+        t.push_back(0);
+        while(j<b.size())
+        {
+		   // match, move two indices together
+            if (b[i]==b[j])
+            {
+                t.push_back(i+1);
+                ++i;
+                ++j;
+            }
+			// i cannot go back anymore, just increase j
+            else if (i==0)
+            {
+                t.push_back(0);
+                ++j;
+            }
+            else
+            {
+			   // move index i back based on table index
+                i = t[i-1];
+            }
+        }
+        
+        i = 0;
+        j = 0;
+        // i traverses the pattern, j traverses the text
+        // j never retraverse, i can go back and forth
+        // this loop is ensentially the same as above but just traversing two strings instead of the same one
+        while (j < a.size() + b.size())
+        {
+            if (i == b.size())
+            {
+                return (j - 1) / a.size() + 1;// this formula is different
+            }
+            // match, move two indices together
+            if (a[j%a.size()] == b[i])
+            {
+                ++j;
+                ++i;
+            }
+			// i cannot go back anymore, just increase j
+            else if (i == 0)
+            {
+                ++j;
+            }
+			// move index i back based on table index
+            else if (i > 0)
+            {
+                i = t[i - 1];
+            }
+        }
+        
+        return -1;
+        
+    }
+};
+
+/*class Solution {
+public:
+    int repeatedStringMatch(string A, string B) {
         for (int i = 0; i < A.length(); i++) {
             if (A[i] == B[0]) {
                 int count = 1;
@@ -20,7 +85,7 @@ public:
         }
         return -1;
     }
-};
+};*/
 
 /*good test cases-
 "abcd"
