58fba06b13b8275b35f340ff84aac073dbd862ec
Sonali Agrawal <sonali9696@gmail.com>
2022-02-13 19:30:39 +0530

    Better space complexity

--- Leetcode Feature chapters/Top Interview Questions - Medium/Word Search.cpp
+++ Leetcode Feature chapters/Top Interview Questions - Medium/Word Search.cpp
@@ -1,4 +1,53 @@
+//without extra space used by visited -
+
 class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        int rows = board.size(), cols = board[0].size();
+        int index = 0;
+        bool found = false;
+        
+        for(int i=0; i<rows; i++)
+        {
+            for(int j=0; j<cols; j++)
+            {
+                dfs(board, word, i, j, index, found);
+            }
+        }
+        
+        return found;
+    }
+    
+    void dfs(vector<vector<char>>& board, string word, int i, int j, int index, bool& found)
+    {   
+        if(i == board.size() || i < 0 || j < 0 || j == board[0].size() || found == true 
+           || board[i][j] == '#' || index == word.size())         
+        {
+            return;
+        }
+        
+        if(board[i][j] == word[index])
+        {
+            board[i][j] = '#'; //i.e. visited
+            
+            if(index == word.size() - 1)
+            {
+                found = true;
+                return;
+            }
+            
+            dfs(board, word, i-1, j, index+1, found); 
+            dfs(board, word, i+1, j, index+1, found);
+            dfs(board, word, i, j-1, index+1, found);
+            dfs(board, word, i, j+1, index+1, found);
+            
+            board[i][j] = word[index]; //reverting back
+        }
+        
+    }
+};
+
+/*class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
         int rows = board.size(), cols = board[0].size();
@@ -54,4 +103,4 @@ public:
         
         visited[i][j] = false;
     }
-};
+};*/
