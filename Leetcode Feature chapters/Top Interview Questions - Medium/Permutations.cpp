ff28f28d7922b90bcddfdccd490087799fcfbab8
Sonali Agrawal <sonali9696@gmail.com>
2022-02-13 12:50:29 +0530

    Permutations solution 2

--- Leetcode Feature chapters/Top Interview Questions - Medium/Permutations.cpp
+++ Leetcode Feature chapters/Top Interview Questions - Medium/Permutations.cpp
@@ -1,4 +1,36 @@
 class Solution {
+private:
+    vector<vector<int>> ans;
+        
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        int index=0, n = nums.size(); //index at which all n numbers will be tried at current iteration
+        createPerm(nums,n,index);
+        
+        return ans;
+    }
+    
+    void createPerm(vector<int> nums, int n, int index)
+    {
+        if(index == n)
+        {
+            ans.push_back(nums);
+            return;
+        }
+        
+        for(int i=index; i<nums.size(); i++)
+        {
+            swap(nums[index], nums[i]);
+            
+            createPerm(nums, n, index+1);
+            
+            swap(nums[index], nums[i]); //get it back to what it was for next iteration
+        }
+    }
+    
+};
+
+/*class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
@@ -30,4 +62,4 @@ public:
             auto it = nums.insert(nums.begin() + i, temp);
         }
     }
-};
+};*/
