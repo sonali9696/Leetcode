f09efe053f0dd95307ae1f5e16cac929e2c5a261
Sonali Agrawal <sonali9696@gmail.com>
2022-02-20 20:16:24 +0530

    explanation added

--- Leetcode Feature chapters/Top Interview Questions - Medium/Sum of Two Integers.cpp
+++ Leetcode Feature chapters/Top Interview Questions - Medium/Sum of Two Integers.cpp
@@ -3,14 +3,26 @@ public:
     int getSum(int a, int b) {
         long mask = 0xFFFFFFFF;
         
+        //if a,b are +ve then it is a+b
+        //if a is negative, b positive then it is -a+b i.e. subtraction
+        
+        //for addition: a xor b gives value of addition/subtraction without carry
+        //carry is (a&b)<<1 for addition, (~a&b)<<1 for subtraction where a>b
+        //so ans is first part + carry which is another sum question
+        //so keep doing it until carry !=0
+        
+        //now instead of handling positive and negative separately we can & it with mask (1111.. 32 times)
+        //this handles both
+        
+        
         while (b != 0) {
-            int answer = (a ^ b) & mask;
+            int answer = (a ^ b) & mask; 
             int carry = ((a & b) & mask) << 1;
             a = answer;
             b = carry;
         }
          
-        return a < INT_MAX ? a : ~(a ^ mask);
+        return a < INT_MAX ? a : ~(a ^ mask); //handles overflow
     }
 };
 /*class Solution {
