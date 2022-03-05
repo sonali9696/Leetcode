bd148a2600f2f80cd443db5821fae4d3d9b0fc0d
Sonali Agrawal <sonali9696@gmail.com>
2022-03-05 19:45:04 +0530

    Validate Binary Search Tree

---
+++ Others/Validate Binary Search Tree.cpp
@@ -0,0 +1,54 @@
+//Approach 2- inorder, check if it is in ascending sorted order
+class Solution {
+public:
+    bool validate(TreeNode* root, TreeNode* low, TreeNode* high) {
+        // Empty trees are valid BSTs.
+        if (root == nullptr) {
+            return true;
+        }
+
+        // The current node's value must be between low and high.
+        if ((low != nullptr and root->val <= low->val) or
+            (high != nullptr and root->val >= high->val)) {
+            return false;
+        }
+
+        // The left and right subtree must also be valid.
+        return validate(root->right, root, high) and
+               validate(root->left, low, root);
+    }
+
+    bool isValidBST(TreeNode* root) {
+        return validate(root, nullptr, nullptr);
+    }
+};
+
+
+//checking if all elements are in the right range
+//just checking if left < root < right is not enough
+//as all elements in right subtree should be > than root even if above is true
+//eg: [5,1,6,null,null,4,7]
+
+/*class Solution {
+public:
+    bool validate(TreeNode* root, TreeNode* low, TreeNode* high) {
+        // Empty trees are valid BSTs.
+        if (root == nullptr) {
+            return true;
+        }
+
+        // The current node's value must be between low and high.
+        if ((low != nullptr and root->val <= low->val) or
+            (high != nullptr and root->val >= high->val)) {
+            return false;
+        }
+
+        // The left and right subtree must also be valid.
+        return validate(root->right, root, high) and
+               validate(root->left, low, root);
+    }
+
+    bool isValidBST(TreeNode* root) {
+        return validate(root, nullptr, nullptr);
+    }
+};*/
