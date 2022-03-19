1ce082c4a3b1a07f8f696f3693562f59fbb03051
Sonali Agrawal <sonali9696@gmail.com>
2022-03-19 19:25:02 +0530

    better space complexity

--- Leetcode Google questions/Remove All Ones With Row and Column Flips.cpp
+++ Leetcode Google questions/Remove All Ones With Row and Column Flips.cpp
@@ -1,4 +1,27 @@
+//better space -
+
 class Solution {
+public:
+    bool removeOnes(vector<vector<int>>& grid) {
+        //we can remove ones if all rows are either the same or opposite of each other
+        if(grid.size() == 0 || grid[0].size() == 0) return true;
+        
+        //check if all rows are either same as row or opposite of it
+        for(int i=1; i<grid.size(); i++)
+        {
+            if(grid[i] == grid[0]) continue;
+            
+            for(int j=0; j<grid[i].size(); j++)
+            {
+                if(grid[i][j] != 1 - grid[0][j]) return false;    
+            }
+        }
+        
+        return true;
+    }
+};
+
+/*class Solution {
 public:
     bool removeOnes(vector<vector<int>>& grid) {
         //we can remove ones if all rows are either the same or opposite of each other
@@ -24,4 +47,4 @@ public:
         
         return true;
     }
-};
+};*/
