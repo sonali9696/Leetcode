695876eea293ec3d517587b65d438c9e8296ebad
Sonali Agrawal <sonali9696@gmail.com>
2022-03-23 16:32:38 +0530

    faster

--- Leetcode Google questions/Longest Arithmetic Subsequence of Given Difference.cpp
+++ Leetcode Google questions/Longest Arithmetic Subsequence of Given Difference.cpp
@@ -1,4 +1,19 @@
+//for faster performance can create fixed size array using constraints-
 class Solution {
+public:
+    int longestSubsequence(vector<int>& arr, int diff) {
+        vector<int>dp(40005);//an alternative of map to improve performance
+        int ans=1;
+        for(auto x:arr){
+            //for neg value add 20000
+            dp[x+20000]=dp[x+20000-diff]+1;
+            ans=max(ans,dp[x+20000]);
+        }
+        return ans;
+    }
+};
+
+/*class Solution {
 public:
     int longestSubsequence(vector<int>& arr, int difference) {
         unordered_map<int, int> dp; //number, length of subsequence upto this number
@@ -27,4 +42,4 @@ public:
         
         return maxLength;
     }
-};
+};*/
