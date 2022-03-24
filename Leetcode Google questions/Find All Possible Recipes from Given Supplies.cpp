a13653fe71c70be7cdfdf0a681a54b7700368b56
Sonali Agrawal <sonali9696@gmail.com>
2022-03-24 17:50:56 +0530

    topological sort solution added

--- Leetcode Google questions/Find All Possible Recipes from Given Supplies.cpp
+++ Leetcode Google questions/Find All Possible Recipes from Given Supplies.cpp
@@ -1,3 +1,47 @@
+//Topological sort - adding nodes of zero indegree to queue and processing them first
+//supplies will initially have zero indegree
+
+/*class Solution {
+public:
+    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
+        queue<string> q;
+		
+		// this stores indegree of each recipes
+        unordered_map<string,int> inorder;
+		//stores all the relations of graph whre it's key will be some supply/recipe need for some recipe
+        unordered_map<string,vector<string>> adj;
+        vector<string> ans;
+		
+		//push every supply element in queue as it's indegree will be already zero
+        for(string s: supplies) q.push(s);
+        
+		
+        for(int i=0; i< recipes.size();i++){
+            for(string str: ingredients[i]){
+			//we are increasing indegree of each recipe according to no. of things needed for it
+                inorder[recipes[i]]++;
+			//we are pushing each recipe corresponding to ingredient 
+                adj[str].push_back(recipes[i]);
+            }
+        }
+        
+        while(!q.empty()){
+            string curr = q.front();
+            q.pop();
+            for(string s: adj[curr]){
+			
+			//checking if indegree of recipe & pushing it in ans and que as it can be ingredient for some other recipe
+                if(--inorder[s]==0){
+                    ans.push_back(s);
+                    q.push(s);
+                }
+            }
+        }
+        return ans;
+    }
+};*/
+
+
 class Solution {
 private:
     unordered_map<string, bool> dp; //ingredient/recipe is available/can be made or not
