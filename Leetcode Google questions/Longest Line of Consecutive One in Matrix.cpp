b9653dd8857473b9ec378ef874a8330a5d2eb7d6
Sonali Agrawal <sonali9696@gmail.com>
2022-03-26 19:09:43 +0530

    improved

--- Leetcode Google questions/Longest Line of Consecutive One in Matrix.cpp
+++ Leetcode Google questions/Longest Line of Consecutive One in Matrix.cpp
@@ -1,4 +1,57 @@
+//Reducing size of dp arrays
+//as only prev row is needed in calculations just store that 
+
 class Solution {
+public:
+    int longestLine(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+        
+        vector<int> horiz(n), vert(n), diag(n), antiDiag(n);
+        
+        int longestLine = 0;
+        
+        for(int i=0; i<m; i++)
+        {
+            int prevDiag = 0;
+            
+            for(int j=0; j<n; j++)
+            {
+                if(mat[i][j] == 1)
+                {
+                    horiz[j] = (j - 1 >= 0)?(horiz[j-1] + 1) : 1;
+                    vert[j] = (i - 1 >= 0)?(vert[j] + 1) : 1; //vert[j] has i-1 value before it is overwritten hence this can be used
+                    
+                    //we cant simply use diag[j-1] because all values before i are already overwritten
+                    int currDiag = diag[j]; //this will be used in next j iteration
+                    //so for that element, this is the value of j-1 before it is overwritten         
+                    diag[j] = (i-1 >= 0 && j-1 >= 0)?(prevDiag + 1) : 1;
+                    prevDiag = currDiag; //for next j
+                    
+                    antiDiag[j] = (i-1 >= 0 && j+1 < n)?(antiDiag[j+1] + 1) : 1;
+                    
+                    int maxLen1 = max(horiz[j], vert[j]);
+                    int maxLen2 = max(diag[j], antiDiag[j]);
+                    int maxLen = max(maxLen1, maxLen2);
+                    
+                    if(longestLine < maxLen) longestLine = maxLen;
+                }
+                else
+                {
+                    prevDiag = diag[j];
+                    
+                    horiz[j] = 0;
+                    vert[j] = 0;
+                    diag[j] = 0;
+                    antiDiag[j] = 0;   
+                }
+            }
+        }
+        
+        return longestLine;
+    }
+};
+
+/*class Solution {
 public:
     int longestLine(vector<vector<int>>& mat) {
         int m = mat.size(), n = mat[0].size();
@@ -32,4 +85,4 @@ public:
         
         return longestLine;
     }
-};
+};*/
